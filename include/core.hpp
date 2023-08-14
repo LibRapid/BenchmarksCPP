aef483e4d6f721aa0bd55d2bceb94440f449296f
Toby Davis <pencilcaseman@gmail.com>
2023-08-13 21:15:47 -0700

    Update

---
+++ include/core.hpp
@@ -0,0 +1,16 @@
+#include <omp.h>
+
+// Include the libraries
+
+// ----- LibRapid ----------------
+#include <librapid>
+
+// ----- eigen -------------------
+#include <Eigen>
+
+// ----- xtensor -----------------
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
+
+#include <nanobench.h>
